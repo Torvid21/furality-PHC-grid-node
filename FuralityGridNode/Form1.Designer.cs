3ec12b7a93771569de7606bcbfbf6a4f67d3abe6
Torvid21 <130079388+Torvid21@users.noreply.github.com>
2025-02-02 23:34:16 +0100

    initial commit

---
+++ FuralityGridNode/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace FuralityGridNode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.helloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configPanel = new System.Windows.Forms.Panel();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.colorTypeDropdown = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portInput = new System.Windows.Forms.TextBox();
+            this.ipInput = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.configPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helloToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(111, 48);
+            // 
+            // helloToolStripMenuItem
+            // 
+            this.helloToolStripMenuItem.Name = "helloToolStripMenuItem";
+            this.helloToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.helloToolStripMenuItem.Text = "Config";
+            this.helloToolStripMenuItem.Click += new System.EventHandler(this.Config_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // configPanel
+            // 
+            this.configPanel.Controls.Add(this.statusLabel);
+            this.configPanel.Controls.Add(this.colorTypeDropdown);
+            this.configPanel.Controls.Add(this.label3);
+            this.configPanel.Controls.Add(this.label2);
+            this.configPanel.Controls.Add(this.label1);
+            this.configPanel.Controls.Add(this.portInput);
+            this.configPanel.Controls.Add(this.ipInput);
+            this.configPanel.Location = new System.Drawing.Point(12, 12);
+            this.configPanel.Name = "configPanel";
+            this.configPanel.Size = new System.Drawing.Size(165, 184);
+            this.configPanel.TabIndex = 1;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(3, 105);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(37, 13);
+            this.statusLabel.TabIndex = 8;
+            this.statusLabel.Text = "Status";
+            // 
+            // colorTypeDropdown
+            // 
+            this.colorTypeDropdown.FormattingEnabled = true;
+            this.colorTypeDropdown.Items.AddRange(new object[] {
+            "VRSL",
+            "Packed"});
+            this.colorTypeDropdown.Location = new System.Drawing.Point(3, 81);
+            this.colorTypeDropdown.Name = "colorTypeDropdown";
+            this.colorTypeDropdown.Size = new System.Drawing.Size(100, 21);
+            this.colorTypeDropdown.TabIndex = 7;
+            this.colorTypeDropdown.Text = "VRSL";
+            this.colorTypeDropdown.SelectedIndexChanged += new System.EventHandler(this.colorTypeDropdown_SelectedIndexChanged);
+            this.colorTypeDropdown.TextChanged += new System.EventHandler(this.inputChanged_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DMX Grid Config";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            // 
+            // portInput
+            // 
+            this.portInput.Location = new System.Drawing.Point(3, 55);
+            this.portInput.Name = "portInput";
+            this.portInput.Size = new System.Drawing.Size(100, 20);
+            this.portInput.TabIndex = 1;
+            this.portInput.Text = "6454";
+            this.portInput.TextChanged += new System.EventHandler(this.inputChanged_TextChanged);
+            // 
+            // ipInput
+            // 
+            this.ipInput.Location = new System.Drawing.Point(3, 29);
+            this.ipInput.Name = "ipInput";
+            this.ipInput.Size = new System.Drawing.Size(100, 20);
+            this.ipInput.TabIndex = 0;
+            this.ipInput.Text = "127.0.0.1";
+            this.ipInput.TextChanged += new System.EventHandler(this.inputChanged_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1920, 208);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.ControlBox = false;
+            this.Controls.Add(this.configPanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Furality Grid Node";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.configPanel.ResumeLayout(false);
+            this.configPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helloToolStripMenuItem;
+        private System.Windows.Forms.Panel configPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox portInput;
+        private System.Windows.Forms.TextBox ipInput;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox colorTypeDropdown;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ FuralityGridNode/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuralityGridNode
+{
+    public partial class Form1 : Form
+    {
+        static int size = 16;
+        static int countY = 13;
+        static int countX = (512 / countY + 1);
+        //static Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            form = this;
+            this.MouseDown += new MouseEventHandler(MainForm_MouseDown);
+            g.Clear(Color.Black);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BITMAPINFOHEADER
+        {
+            public uint biSize;
+            public int biWidth;
+            public int biHeight;
+            public ushort biPlanes;
+            public ushort biBitCount;
+            public uint biCompression;
+            public uint biSizeImage;
+            public int biXPelsPerMeter;
+            public int biYPelsPerMeter;
+            public uint biClrUsed;
+            public uint biClrImportant;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RGBQUAD
+        {
+            public byte rgbBlue;
+            public byte rgbGreen;
+            public byte rgbRed;
+            public byte rgbReserved;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BITMAPINFO
+        {
+            public BITMAPINFOHEADER bmiHeader;
+            public RGBQUAD bmiColors;
+        }
+
+        [DllImport("gdi32.dll")]
+        public static extern int StretchDIBits(
+            IntPtr hdc,
+            int xDest,
+            int yDest,
+            int DestWidth,
+            int DestHeight,
+            int xSrc,
+            int ySrc,
+            int SrcWidth,
+            int SrcHeight,
+            IntPtr lpBits,
+            ref BITMAPINFO lpBitsInfo,
+            uint iUsage,
+            uint dwRop);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        // Define the necessary constants
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+        public static Graphics g;
+        public static Form1 form;
+        static byte[] combinedData = new byte[512*8];
+
+        static int listenPort = 6454;
+        static IPAddress listenAddress = IPAddress.Loopback;
+        static UdpClient listener;
+        static Thread listenerThread;
+        private static void StartListener()
+        {
+            statusText = "listener started";
+            if (listener != null)
+                listener.Close();
+            try
+            {
+                listener = new UdpClient(listenPort);
+            }
+            catch(Exception e)
+            {
+                statusText = e.Message;
+                return;
+            }
+            
+            IPEndPoint groupEP = new IPEndPoint(listenAddress, listenPort);
+
+            while (true)
+            {
+                if (listenAddress == null)
+                    return;
+
+                statusText = "listening on " + listenAddress.ToString() + ":" + listenPort;
+                byte[] bytes = listener.Receive(ref groupEP);
+
+                int opcode = (bytes[8 + 1] << 8) | bytes[8 + 0];
+                if (opcode == 0x5000)
+                {
+                    int universe = (bytes[12 + 3] << 8) | bytes[12 + 2];
+                    if (universe < 8)
+                    {
+                        for (int i = 0; i < 512; i++)
+                        {
+                            combinedData[i + universe * 512] = bytes[i + 18];
+                        }
+                        update = true;
+                    }
+                }
+            }
+
+            listener.Close();
+        }
+
+        static bool update = false;
+        static string statusText = "";
+        static string statusTextLast = "";
+
+        void DrawSquare(byte[] data, int dataSizeX, int dataSizeY, int X, int Y, int sizeX, int sizeY, byte R, byte G, byte B)
+        {
+            for (int x = X; x < X + sizeX; x++)
+            {
+                for (int y = Y; y < Y + sizeY; y++)
+                {
+                    int index = (x + y * dataSizeX) * 3;
+                    data[index + 0] = B; // B
+                    data[index + 1] = G;
+                    data[index + 2] = R;
+                }
+            }
+        }
+
+        public void DrawData()
+        {
+            int sizeX = countX * 16 * 3;
+            int sizeY = countY * 16;
+            byte[] output = new byte[sizeX * sizeY * 3];
+
+            string selectedItem = colorTypeDropdown.SelectedItem as string;
+
+            if (selectedItem == "VRSL")
+            {
+                for (int universe = 0; universe < 3; universe++)
+                {
+                    for (int i = 0; i < 512; i++)
+                    {
+                        byte data = combinedData[i + universe * 512];
+                        int x = i / 13;
+                        int y = i % 13;
+                        DrawSquare(output, sizeX, sizeY, (x + universe * countX) * 16, y * 16, 16, 16, data, data, data);
+                    }
+                }
+            }
+            else if (selectedItem == "Packed")
+            {
+                for (int i = 0; i < ((512*8) / 3); i++)
+                {
+                    byte dataR = combinedData[i * 3 + 0];
+                    byte dataG = combinedData[i * 3 + 1];
+                    byte dataB = combinedData[i * 3 + 2];
+
+                    int x = i / 13;
+                    int y = i % 13;
+                    DrawSquare(output, sizeX, sizeY, x * 16, y * 16, 16, 16, dataR, dataG, dataB);
+                }
+            }
+            if (output == null)
+                return;
+
+            // win32 fast draw byte[] to a control. Idk why the default C# functions are so disgustingly slow.
+            GCHandle pinnedArray = GCHandle.Alloc(output, GCHandleType.Pinned);
+            IntPtr pointer = pinnedArray.AddrOfPinnedObject();
+            IntPtr hdc = g.GetHdc();
+            BITMAPINFO bmi = new BITMAPINFO();
+            bmi.bmiHeader = new BITMAPINFOHEADER
+            {
+                biSize = (uint)Marshal.SizeOf(typeof(BITMAPINFOHEADER)),
+                biWidth = sizeX,
+                biHeight = -sizeY, // Negative height to indicate a top-down DIB
+                biPlanes = 1,
+                biBitCount = 24,
+                biCompression = 0, // BI_RGB
+                biSizeImage = (uint)(sizeY * sizeX)
+            };
+            StretchDIBits(hdc, 0, 0, sizeX, sizeY, 0, 0, sizeX, sizeY, pointer, ref bmi, 0, 0x00CC0020);
+            g.ReleaseHdc(hdc);
+            pinnedArray.Free();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RestartClient();
+        }
+
+        void RestartClient()
+        {
+            statusText = "listener starting";
+
+            int.TryParse(portInput.Text, out listenPort);
+            IPAddress.TryParse(ipInput.Text, out listenAddress);
+
+            if (listenAddress == null)
+            {
+                statusText = "invalid IP";
+                return;
+            }
+
+            if (listenPort == 0)
+            {
+                statusText = "invalid Port";
+                return;
+            }
+
+            if (listenerThread != null)
+                listenerThread.Abort();
+
+            listenerThread = new Thread(new ThreadStart(StartListener));
+            listenerThread.Start();
+        }
+
+        private void Config_Click(object sender, EventArgs e)
+        {
+            configPanel.Visible = !configPanel.Visible;
+            Application.DoEvents();
+            DrawData();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (update)
+            {
+                update = false;
+                DrawData();
+            }
+
+            if (statusTextLast != statusText)
+            {
+                statusLabel.Text = statusText;
+                statusTextLast = statusText;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void colorTypeDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DrawData();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void inputChanged_TextChanged(object sender, EventArgs e)
+        {
+            RestartClient();
+            DrawData();
+        }
+    }
+}
